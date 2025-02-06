9de025263df7c72b9bdf1fd42b41f41f9961b5fe
Rx-78 <149854255+rx-72@users.noreply.github.com>
2025-02-05 22:04:48 -0800

    Update run.py

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ METRICS = {
     "accuracy": accuracy
 }
 def normalization_all(X_train_ins, X_test_ins, y_train_ins, y_test_ins, X_train_mpg, X_test_mpg, y_train_mpg, y_test_mpg, X_train_bos, X_test_bos, y_train_bos, y_test_bos, X_train_fire, X_test_fire, y_train_fire, y_test_fire):
-  Print("Getting things started...")
+  print("Getting things started...")
   X_train_transformed = X_train_ins.copy()
   X_test_transformed = X_test_ins.copy()
 
