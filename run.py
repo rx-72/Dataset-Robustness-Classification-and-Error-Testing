6a46ef625d31436e370cf8b401a2ca712d9594a8
Rx-78 <149854255+rx-72@users.noreply.github.com>
2025-02-12 16:17:50 -0800

    Update run.py

--- run.py
+++ run.py
@@ -117,7 +117,10 @@ def pattern_mining(X_train, X_test, y_train, y_test, column_2_bins, num_bins, pa
 
   candidates = compute_optimized_candidates(X_train_transformed, 0.1, n_features_to_combine=pattern_size)
   if args != None:
+    
     candidates[9892] = ((3, 4, 5), (9, 3, 82.0), ('<', '<', '<'))
+    candidates[9050] = ((1, 3, 6), (0, 3, 1.0), ('>', '>', '>'))
+    candidates[9051] = ((3, 4, 5), (4, 1, 78.0), ('>', '>', '>'))
   
   def get_complex_candidate_target_indices(df, candidate):
     columns, values, conditions = candidate
@@ -222,12 +225,12 @@ def pattern_mining(X_train, X_test, y_train, y_test, column_2_bins, num_bins, pa
 
   result = top_k_finder(top_candidates, X_train_transformed, X_train, y_train, X_test, y_test, 10, 0.1, rr, ur)
 
-  print("")
-  print(result)
-  print("")
+  #print("")
+  #print(result)
+  #print("")
   
   lst = []
-  threshold_count = len(X_train)*0.1*0.25
+  threshold_count = len(X_train)*0.1*0.5
   for i in result:
     def matchcounter(x, y):
         count = 0
